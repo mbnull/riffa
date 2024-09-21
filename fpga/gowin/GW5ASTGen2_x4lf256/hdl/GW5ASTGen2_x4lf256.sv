29c3fb232d0f582101725881b9f96abe7b9c2ae5
bnull <pointnextworld@outlook.com>
2024-09-22 01:53:58 +0800

    improve frame work

--- fpga/gowin/GW5ASTGen2_x4lf256/hdl/GW5ASTGen2_x4lf256.sv
+++ fpga/gowin/GW5ASTGen2_x4lf256/hdl/GW5ASTGen2_x4lf256.sv
@@ -3,7 +3,7 @@
 `include "tlp.vh"
 `include "gowin.vh"
 
-module GW5AST_Gen2_x4lf256
+module GW5ASTGen2_x4lf256
     #(
     parameter C_NUM_CHNL = 1,
     parameter C_NUM_LANES = 4,
@@ -11,13 +11,13 @@ module GW5AST_Gen2_x4lf256
     parameter C_MAX_PAYLOAD_BYTES = 1024,
     parameter C_LOG_NUM_TAGS = 5
 )(
-    input PCIE_REFCLK,
-    input PCIE_REF_RST_N,
+    input clk,
+    input rst_n,
     output [3:0] LED
 );
 
 
-    
+
     SerDes_Top pcie_serder_top(
         .PCIE_Controller_Top_pcie_tl_rx_sop_o(PCIE_Controller_Top_pcie_tl_rx_sop_o_o), //output PCIE_Controller_Top_pcie_tl_rx_sop_o
         .PCIE_Controller_Top_pcie_tl_rx_eop_o(PCIE_Controller_Top_pcie_tl_rx_eop_o_o), //output PCIE_Controller_Top_pcie_tl_rx_eop_o
@@ -46,4 +46,46 @@ module GW5AST_Gen2_x4lf256
         .PCIE_Controller_Top_pcie_tl_int_msinum_i(PCIE_Controller_Top_pcie_tl_int_msinum_i_i) //input [4:0] PCIE_Controller_Top_pcie_tl_int_msinum_i
     );
 
+    GW5AST_wrapper riffa();
+
+    logic rx_clk;
+
+    logic channel_rx;
+    logic channel_rx_ack;
+    logic channel_rx_last;
+    logic channel_rx_len;
+    logic channel_rx_off;
+    logic channel_rx_data;
+    logic channeL_rx_data_valid;
+    logic channel_rx_data_ren;
+
+    // user logic
+    chnl_tester
+    #(
+    .C_PCI_DATA_WIDTH(C_PCI_DATA_WIDTH)
+    )
+    module1
+    (.CLK(clk),
+        .RST(rst_n), // riffa_reset includes riffa_endpoint resets
+        // Rx interface
+        .CHNL_RX_CLK(rx_clk),
+        .CHNL_RX(channel_rx),
+        .CHNL_RX_ACK(channel_rx_ack),
+        .CHNL_RX_LAST(chnl_rx_last[chnl]),
+        .CHNL_RX_LEN(chnl_rx_len[32*chnl +:32]),
+        .CHNL_RX_OFF(chnl_rx_off[31*chnl +:31]),
+        .CHNL_RX_DATA(chnl_rx_data[C_PCI_DATA_WIDTH*chnl +:C_PCI_DATA_WIDTH]),
+        .CHNL_RX_DATA_VALID(chnl_rx_data_valid[chnl]),
+        .CHNL_RX_DATA_REN(chnl_rx_data_ren[chnl]),
+        // Tx interface
+        .CHNL_TX_CLK(chnl_tx_clk[chnl]),
+        .CHNL_TX(chnl_tx[chnl]),
+        .CHNL_TX_ACK(chnl_tx_ack[chnl]),
+        .CHNL_TX_LAST(chnl_tx_last[chnl]),
+        .CHNL_TX_LEN(chnl_tx_len[32*chnl +:32]),
+        .CHNL_TX_OFF(chnl_tx_off[31*chnl +:31]),
+        .CHNL_TX_DATA(chnl_tx_data[C_PCI_DATA_WIDTH*chnl +:C_PCI_DATA_WIDTH]),
+        .CHNL_TX_DATA_VALID(chnl_tx_data_valid[chnl]),
+        .CHNL_TX_DATA_REN(chnl_tx_data_ren[chnl])
+    );
 endmodule
---
+++ fpga/riffa_hdl/gowin.vh
@@ -0,0 +1,6 @@
+`ifndef GOWIN_VH
+`define GOWIN_VH 1
+
+
+
+`endif
