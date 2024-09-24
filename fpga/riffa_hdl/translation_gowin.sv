a175ef14430a962dda3131f7b46ab5564dcbf99c
bnull <pointnextworld@outlook.com>
2024-09-25 00:24:29 +0800

    finish gowin translation

--- fpga/riffa_hdl/translation_gowin.sv
+++ fpga/riffa_hdl/translation_gowin.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 module translation_gowin
-    #(
+#(
   parameter C_PCI_DATA_WIDTH = 256
 )
     (
@@ -18,25 +18,23 @@ module translation_gowin
 
 
   // Interface: Gowin TX
-  output [C_PCI_DATA_WIDTH-1:0]  TX_ST_DATA,
-  output [0:0]                   TX_ST_VALID,
-  input                          TX_ST_READY,
-  output [0:0]                   TX_ST_EOP,
-  output [0:0]                   TX_ST_SOP,
-  output [0:0]                   TX_ST_EMPTY,
+  output [C_PCI_DATA_WIDTH-1:0]  TL_TX_DATA,
+  output [7:0]                   TL_TX_VALID,
+  input                          TL_TX_WAIT,
+  output [0:0]                   TL_TX_EOP,
+  output [0:0]                   TL_TX_SOP,
 
-  // Interface: Altera Config
-  //  input [`SIG_CFG_CTL_W-1:0]     TL_CFG_CTL,
-  //  input [`SIG_CFG_ADD_W-1:0]     TL_CFG_ADD,
-  //  input [`SIG_CFG_STS_W-1:0]     TL_CFG_STS,
+  // Interface: Gowin Config
+  input [12:0]                   TL_CFG_BUSDEV,
 
-  // Interface: Altera Flow Control
-  //  input [`SIG_FC_CPLH_W-1:0]     KO_CPL_SPC_HEADER,
-  //  input [`SIG_FC_CPLD_W-1:0]     KO_CPL_SPC_DATA,
+  // Interface: Gowin Flow Control
+  input [31:0]                   TL_TX_CPL,
 
   // Interface: Gowin Interrupt
   input                          APP_MSI_ACK,
   output                         APP_MSI_REQ,
+  output                         APP_MSI_EN,
+  output [4:0]                   APP_MSI_MSINUM,
 
   // Interface: RX Classic
   output [C_PCI_DATA_WIDTH-1:0]            RX_TLP,
@@ -66,6 +64,8 @@ module translation_gowin
   output [`SIG_MAXPAYLOAD_W-1:0] CONFIG_MAX_PAYLOAD_SIZE,
   output                         CONFIG_INTERRUPT_MSIENABLE,
   output                         CONFIG_CPL_BOUNDARY_SEL,
+
+  // Interface: Flow Control
   output [`SIG_FC_CPLD_W-1:0]    CONFIG_MAX_CPL_DATA, // Receive credit limit for data
   output [`SIG_FC_CPLH_W-1:0]    CONFIG_MAX_CPL_HDR, // Receive credit limit for headers
 
@@ -74,10 +74,7 @@ module translation_gowin
   input                          INTR_MSI_REQUEST // High to request interrupt
 );
 
-  assign CONFIG_LINK_WIDTH = 6'b000100;
-  assign CONFIG_LINK_RATE = 4'b0010;
-  assign CONFIG_MAX_PAYLOAD_SIZE  = 3'b010;
-
+  // {{{ Gowin TX
   assign RX_TLP = TL_RX_DATA;
   assign RX_TLP_VALID = |TL_RX_VALID;
   assign RX_TLP_START_OFFSET = 'b0;
@@ -98,6 +95,54 @@ module translation_gowin
     endcase
   end
   assign TL_RX_WAIT = RX_TLP_READY;
+  // }}}
+
+  // {{{ Gowin RX
+  logic rTxStReady;
+  logic [7:0] TxExtend;
+  assign TxExtend = {8{TX_TLP_VALID}};
+  always_ff@(posedge CLK)begin
+    rTxStReady <= ~TL_TX_WAIT&|TL_TX_VALID;
+  end
+  assign TL_TX_DATA = TX_TLP;
+  assign TL_TX_SOP = TX_TLP_START_FLAG;
+  assign TL_TX_EOP = TX_TLP_END_FLAG;
+  assign TX_TLP_READY = rTxStReady; // wait means activate in low
+  assign TL_TX_VALID = TxExtend[7-TX_TLP_START_OFFSET[2:0]:TX_TLP_END_OFFSET[2:0]];
+  // }}}
+
+  // {{{ Gowin MSI 
+  logic rMsien;
+  assign INTR_MSI_RDY = APP_MSI_ACK;
+  assign APP_MSI_REQ = INTR_MSI_REQUEST;
+  assign APP_MSI_EN = rMsien | INTR_MSI_REQUEST;
+  assign APP_MSI_MSINUM = 'b0;
+  always_ff@(posedge CLK)begin
+    if(!RST_IN)begin
+      rMsien <= 1'b0;
+    end else if(INTR_MSI_REQUEST) begin
+      rMsien <= 1'b1;
+    end else if (APP_MSI_ACK) begin
+      rMsien <= 1'b0;
+    end
+  end
+  // }}}
+
+  // {{{ Gowin Config
+  assign CONFIG_LINK_WIDTH = 6'b000100;
+  assign CONFIG_LINK_RATE = 4'b0010;
+  assign CONFIG_MAX_PAYLOAD_SIZE  = 3'b010;
+  assign CONFIG_MAX_READ_REQUEST_SIZE = 3'b010;
+  assign CONFIG_COMPLETER_ID = {TL_CFG_BUSDEV,3'b000};
+  assign CONFIG_BUS_MASTER_ENABLE = 1'b1;
+  assign CONFIG_INTERRUPT_MSIENABLE = 1'b1;
+  assign CONFIG_CPL_BOUNDARY_SEL = 1'b1;
+  // }}}
+
+  // {{{ Gowin Flow Controller
+  assign CONFIG_MAX_CPL_DATA = TL_TX_CPL[12:0];
+  assign CONFIG_MAX_CPL_HDR = TL_TX_CPL[24:16];
+  // }}}
 
 endmodule // translation_layer
 
